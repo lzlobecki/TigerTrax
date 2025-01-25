0c3fb0a76f4f9571b0ce05ddbc72ab45df5e35d5
lzlobecki <leon.zlobecki@gmail.com>
2025-01-24 19:55:58 -0500

    Place pins for starting and ending class

--- js/script.js
+++ js/script.js
@@ -8,9 +8,29 @@ window.addEventListener("DOMContentLoaded", () => {
     mapImage.onload = () => {
       ctx.drawImage(mapImage, 0, 0, canvas.width, canvas.height);
     };
-  });  
 
-document.getElementById('submitBtn').addEventListener('click', function() {
-    const fromClass = document.getElementById('fromClass').value;
-    const toClass = document.getElementById('toClass').value;
+    document.getElementById('submitBtn').addEventListener('click', function() {
+        const fromClass = document.getElementById('fromClass').value;
+        const toClass = document.getElementById('toClass').value;
+        fetch('pins/A.json')
+            .then(response => response.json())
+            .then(data => {
+                const fromPin = data.find(pin => pin.label === fromClass);
+                const toPin = data.find(pin => pin.label === toClass);
+
+                if (fromPin) {
+                    placePin(fromPin.x, fromPin.y, 'red');
+                }
+                if (toPin) {
+                    placePin(toPin.x, toPin.y, 'blue');
+                }
+            });
+    });
+
+    function placePin(x, y, color) {
+        ctx.fillStyle = color;
+        ctx.beginPath();
+        ctx.arc(x, y, 5, 0, 2 * Math.PI);
+        ctx.fill();
+    }
 });
